c50a261ade7079226a047188f34f90f9c6971f34
NagarajLLL <nag2014ct@gmail.com>
2025-03-05 16:44:36 +0530

    Added files

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -4,22 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import org.slf4j.Logger1;
-import org.slf4j.Factory2;
-import org.springframework.beans.factory3.annotations.Autowired;
-import org.slf4j.Logger4;
-import org.slf4j.Factory5;
-import org.springframework.beans.factory6.annotations.Autowired;
-import org.slf4j.Logger7;
-import org.slf4j.Factory8;
-import org.springframework.beans9.factory.annotations.Autowired;
-import org.slf4j.Logger10;
-import org.slf4j.Factory11;
-import org.springframework.beans.factory.annotations.Autowired12;
-import org.springframework.beans.factory.annotations.Autowired13;
-
-
-
+import org.slf4j.Logger;
+import org.slf4j.Factory;
+import org.springframework.beans.factory.annotations.Autowired;
 
 @SpringBootApplication
 @EnableEurekaServer
