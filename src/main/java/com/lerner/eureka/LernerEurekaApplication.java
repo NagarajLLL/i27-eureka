214bb2471f652e68811a4cfd7155c98f2f294bc1
NagarajLLL <nag2014ct@gmail.com>
2025-03-05 17:00:32 +0530

    Added files

--- src/main/java/com/lerner/eureka/LernerEurekaApplication.java
+++ src/main/java/com/lerner/eureka/LernerEurekaApplication.java
@@ -7,6 +7,19 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.slf4j.Logger;
 import org.slf4j.Factory;
 import org.springframework.beans.factory.annotations.Autowired;
+import org.springframework.beans.factory.annotations.value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.beans.context.annotation.Bean;
+import org.springframework.beans.http.ResponseEntiry;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+
 
 @SpringBootApplication
 @EnableEurekaServer
